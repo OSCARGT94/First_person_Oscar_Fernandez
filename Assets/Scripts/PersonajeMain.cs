1b9d31f63464b4b39b33215e2cf730589e12809e
Oscar <oscarfernan94@gmail.com>
2024-12-01 18:30:15 +0100

    Arreglos menores mientras me lio con el salto

--- Assets/Scripts/PersonajeMain.cs
+++ Assets/Scripts/PersonajeMain.cs
@@ -9,6 +9,9 @@ public class PersonajeMain : MonoBehaviour
     [Header("Movimiento")]
     [SerializeField] float velocidad;
     [SerializeField] float gravedad;
+    [SerializeField] float alturaSalto;
+
+
     [Header("Dtección suelo")]
     [SerializeField] float radioDeteccion;
     [SerializeField] Transform pies;
@@ -30,13 +33,23 @@ public class PersonajeMain : MonoBehaviour
     {
         moveryRotar();
         AplicarGravedad();
+
         if (enSuelo())
         {
             //cancelamos la grabedad cuando aterrizamos.
-
             movVertical.y = 0;
+            saltar();
         }
         
+    }
+
+    private void saltar()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //Aplicamos formula matematica para usarla la fuerza de salto que quiera al tocar suelo.
+            movVertical.y = Mathf.Sqrt(-2 * gravedad * alturaSalto);
+        }
     }
 
     private void moveryRotar()
@@ -46,6 +59,8 @@ public class PersonajeMain : MonoBehaviour
 
         Vector3 input = new Vector3(x, 0, z).normalized;
 
+        //Orientación del cuerpo.
+        transform.rotation = Quaternion.Euler(0, Camera.main.transform.eulerAngles.y, 0);
 
         // Si el juegadopr toca las teclas.
         if (input.magnitude > 0)
@@ -54,8 +69,7 @@ public class PersonajeMain : MonoBehaviour
             //Calculo angulo al que rotarme.
             float anguloRotacion = Mathf.Atan2(input.x, input.z) * Mathf.Rad2Deg + Camera.main.transform.rotation.eulerAngles.y;
 
-            //Orientación del cuerpo.
-            transform.eulerAngles = new Vector3(0, anguloRotacion, 0);
+           
 
             //Mi movimimiento a quedado rotado en base al angulo calculado.
             Vector3 movimiento =  Quaternion.Euler(0, anguloRotacion,0) * Vector3.forward;
@@ -64,22 +78,22 @@ public class PersonajeMain : MonoBehaviour
             controller.Move(movimiento * velocidad * Time.deltaTime);
         }
         
-
-
-
     }
+
     void AplicarGravedad()
     {
         //Mi velocidad vertical va en aumento a cieto factor por segundo.
         movVertical.y += gravedad * Time.deltaTime;
         controller.Move( movVertical * Time.deltaTime);
     }
+
     bool enSuelo()
     {
         //Tirar una esfera de detección en los pies con cierto radio.
         bool suelo = Physics.CheckSphere(pies.position, radioDeteccion, queEsSuelo);
         return suelo;
     }
+
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.green;
